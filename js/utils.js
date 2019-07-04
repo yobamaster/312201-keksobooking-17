c0b51aebbdf724b247c2374a48d6a15527125ab9
xchaikax <artem.akhaltsev@gmail.com>
2019-07-04 14:13:35 +0300

    Выносит сообщения об успехе/ошибке в отдельный модуль

--- js/utils.js
+++ js/utils.js
@@ -2,28 +2,12 @@
 
 (function () {
 
-  var successPopupTemplate = document.querySelector('#success').content.querySelector('.success');
-  var errorPopupTemplate = document.querySelector('#error').content.querySelector('.error');
-  var main = document.querySelector('main');
-
-  var showSuccess = function () {
-    var successPopup = successPopupTemplate.cloneNode(true);
-    main.appendChild(successPopup);
-  };
-
-  var showError = function () {
-    var errorPopup = errorPopupTemplate.cloneNode(true);
-    main.appendChild(errorPopup);
-  };
-
   var getRandomElement = function (elements) {
     return elements[Math.floor(Math.random() * elements.length)];
   };
 
   window.utils = {
-    getRandomElement: getRandomElement,
-    showSuccess: showSuccess,
-    showError: showError
+    getRandomElement: getRandomElement
   };
 
 })();
